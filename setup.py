72dbf9f510f9c8b6ae3318751936942b8bcd512c
Hari-Hrk <hrk.harish87@gmail.com>
2025-05-02 16:56:36 +0530

    requirements.txt done

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import find_packages,setup
 from typing import List
 
+def get_requirements() -> List[str]:  
+    requirements_list: List[str] = []  
+    return requirements_list
 setup(
     name='sensor',
     version='0.0.1',
     author="harish",
     author_email='hrk@gmail.com',
     packages=find_packages(),
-    install_requires = []
+    install_requires = get_requirements()
 )
